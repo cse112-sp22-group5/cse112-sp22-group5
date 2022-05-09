44509ea808626ed03ed7a013b13e065e232d39a7
Them Dang <dnthem@gmail.com>
2022-05-08 22:49:31 -0700

    removed unnecessary line

--- source/modules/muli-language.js
+++ source/modules/muli-language.js
@@ -1,5 +1,4 @@
 export function googleTranslateElementInit() {
-    const tag = document.getElementById("google_translate_element");
     new google.translate.TranslateElement({
     pageLanguage: 'en'
     }, 'google-translate-element');
