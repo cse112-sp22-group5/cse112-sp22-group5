16a60ebb7c5f67de310c4d2937d3141042f6388c
Baha Keskin <bmkeskin@ucsd.edu>
2022-05-24 02:23:59 -0700

    Added audio files to cache

--- source/serviceWorker.js
+++ source/serviceWorker.js
@@ -7,7 +7,12 @@ const assets = [
   "./img/tomato.ico",
   "./img/settings-icon.png",
   "./img/dream-team-logo.png",
-  "./img/icons/delete.png",
+  "./audio/HoliznaCC0-Ghosts.mp3",
+  "./audio/HoliznaCC0-Letting-Go-Of-The-Past.mp3",
+  "./audio/notification-alert-1.wav",
+  "./audio/notification-alert-2.wav",
+  "./audio/notification-alert-3.wav",
+  "./audio/Waltz-Tchaikovsky-Op40.mp3",
 ];
 
 self.addEventListener("install", (installEvent) => {
