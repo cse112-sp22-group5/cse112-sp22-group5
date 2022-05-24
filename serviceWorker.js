5a763db2f12b1e9ced25336da8d056d498b7141b
Baha Keskin <bmkeskin@ucsd.edu>
2022-05-24 11:26:53 -0700

    Changed location of index.html

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,7 @@
 const staticProductoro = "productoroProd";
 const assets = [
-  "./source/index.html",
+  "./",
+  "./index.html",
   "./source/styles.css",
   "./source/main.js",
   "./source/img/tomato.png",
@@ -8,12 +9,21 @@ const assets = [
   "./source/img/settings-icon.png",
   "./source/img/dream-team-logo.png",
   "./source/img/icons/delete.png",
+  "./source/audio/HoliznaCC0-Ghosts.mp3",
+  "./source/audio/HoliznaCC0-Letting-Go-Of-The-Past.mp3",
+  "./source/audio/notification-alert-1.wav",
+  "./source/audio/notification-alert-2.wav",
+  "./source/audio/notification-alert-3.wav",
+  "./source/audio/Waltz-Tchaikovsky-Op40.mp3",
 ];
 
 self.addEventListener("install", (installEvent) => {
   installEvent.waitUntil(
     caches.open(staticProductoro).then((cache) => {
-      cache.addAll(assets);
+      assets.forEach((element) => {
+        console.log(element);
+        cache.add(element);
+      });
     })
   );
 });
