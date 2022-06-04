a82589e7d6a667b9dc7dc82fa444946a6a07a2fa
‘Evan <puzonevan@gmail.com>
2022-06-04 13:48:17 -0700

    color-change refactor

--- tests/unit/color-change.test.js
+++ tests/unit/color-change.test.js
@@ -258,23 +258,6 @@ describe("Test colorChange function", () => {
     ).toBe("short");
   }),
     test("changes the background color at long break state", () => {
-      document.body.innerHTML = `
-            <div id='timer-display' state='pomo'>25:00</div>
-            <section class="tasks" id="tasks">
-                <h2>Tasks</h2>
-
-                <!-- List Options -->
-                <div id="task-options">
-                    <button type="button" id="add-tasks-button">Add</button>
-                    <button type="button" id="clear-tasks-button">Clear</button>
-                    <button type="button" id="clear-completed-tasks-button">Completed</button>
-                </div>
-
-                <hr />
-
-                <ul id="task-list"></ul>
-            </section>
-        `;
       timer.currState = LONG_STATE;
       colorChange();
       expect(document.body.getAttribute("data-state")).toBe("long");
@@ -283,33 +266,6 @@ describe("Test colorChange function", () => {
       ).toBe("long");
     }),
     test("changes the background color at work state", () => {
-      document.body.innerHTML = `
-            <div id='timer-display' state='pomo'>25:00</div>
-            <div class='progress-container'>
-                <div class='circle pomo'></div>
-                <div class='circle short'></div>
-                <div class='circle pomo'></div>
-                <div class='circle short'></div>
-                <div class='circle pomo'></div>
-                <div class='circle short'></div>
-                <div class='circle pomo'></div>
-                <div class='circle long'></div>
-            </div>
-            <section class="tasks" id="tasks">
-                <h2>Tasks</h2>
-
-                <!-- List Options -->
-                <div id="task-options">
-                    <button type="button" id="add-tasks-button">Add</button>
-                    <button type="button" id="clear-tasks-button">Clear</button>
-                    <button type="button" id="clear-completed-tasks-button">Completed</button>
-                </div>
-
-                <hr />
-
-                <ul id="task-list"></ul>
-            </section>
-        `;
       timer.currState = WORK_STATE;
       colorChange();
       expect(document.body.getAttribute("data-state")).toBe("pomo");
