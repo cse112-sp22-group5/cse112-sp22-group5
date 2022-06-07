b6e96d8b4426cab129401f58ac476ef8a30ceef3
Them Dang <dnthem@gmail.com>
2022-06-06 18:26:49 -0700

    fixed keyboard shotcut

--- source/main.js
+++ source/main.js
@@ -40,7 +40,7 @@ document.getElementById("reset-button").addEventListener("click", onReset);
 // Keyboard shortcuts
 document.addEventListener("keydown", (event) => {
   // only allow this event to be fired when task form is hidden
-  if (document.getElementById("tasks-div").style.width === 0)
+  if (document.activeElement !== document.querySelector("#task-name"))
     keyboardShortcut(event);
 });
 // document.getElementById('customize-start').addEventListener('click', customizeKey);
